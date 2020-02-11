dd03b958c3c17ffcc93a099c2749bf7a86faef3e
edison-cy-yang <edison.cy.yang@gmail.com>
2020-02-11 23:15:24 +0000

    product details feature spec to click into product detail page

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to a product from the home page", type: :feature, js: true do
+  #Setup
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+  scenario "They see one product detail" do
+    visit root_path
+    save_screenshot
+
+    page.first('article.product img').click
+    page.find('.products-show')
+    save_screenshot
+    
+  end
+end
